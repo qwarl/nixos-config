d880d21c35b2d0f379b0e91d73a162cbead07e51
qwarl <quannngoc98@gmail.com>
2026-03-23 01:11:35 +0700

    update hyprland stuffs
    - update keybind breaking changes
    - tweak hyprpaper to make it works

--- modules/de-wm/hyprland/settings/hyprpaper/default.nix
+++ modules/de-wm/hyprland/settings/hyprpaper/default.nix
@@ -2,8 +2,10 @@
   services.hyprpaper = {
     enable = true;
     settings = {
+      ipc = "on";
+      splash = false;
       preload = ["/mnt/common/nixos/modules/de-wm/hyprland/wallpapers/daniil-silantev-3pW91fGAKiE-unsplash.jpg"];
-      wallpaper = [", /mnt/common/nixos/modules/de-wm/hyprland/wallpapers/daniil-silantev-3pW91fGAKiE-unsplash.jpg"];
+      wallpaper = [",/mnt/common/nixos/modules/de-wm/hyprland/wallpapers/daniil-silantev-3pW91fGAKiE-unsplash.jpg"];
     };
   };
 }
