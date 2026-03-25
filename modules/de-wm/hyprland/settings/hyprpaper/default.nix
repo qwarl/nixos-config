928d84cce45cb8d69c76f403f4b7931f37acbf0d
qwarl <quannngoc98@gmail.com>
2026-03-25 22:46:15 +0700

    update hyprpaper (breaking change again)

--- modules/de-wm/hyprland/settings/hyprpaper/default.nix
+++ modules/de-wm/hyprland/settings/hyprpaper/default.nix
@@ -4,8 +4,13 @@
     settings = {
       ipc = "on";
       splash = false;
-      preload = ["/mnt/common/nixos/modules/de-wm/hyprland/wallpapers/daniil-silantev-3pW91fGAKiE-unsplash.jpg"];
-      wallpaper = [",/mnt/common/nixos/modules/de-wm/hyprland/wallpapers/daniil-silantev-3pW91fGAKiE-unsplash.jpg"];
+      wallpaper = [
+        {
+          monitor = "";
+          path = "/mnt/common/nixos/modules/de-wm/hyprland/wallpapers/daniil-silantev-3pW91fGAKiE-unsplash.jpg";
+          fit_mode = "cover";
+        }
+      ];
     };
   };
 }
